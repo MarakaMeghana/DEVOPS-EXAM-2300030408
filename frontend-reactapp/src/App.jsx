bf1e826d21258eabca85a0df34b6e1b40a04d3cb
Meghana <marakamegha2004@gmail.com>
2025-10-18 13:38:58 +0530

    updated

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   return (
