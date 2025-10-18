41cd2c26cda735a4d9c3d14020e32afd15bd7f50
Meghana <marakamegha2004@gmail.com>
2025-10-18 13:43:29 +0530

    updated code

--- frontend-reactapp/config.js
+++ frontend-reactapp/config.js
@@ -1,6 +1,6 @@
 const config = {
   // change the port if your backend runs on another
-  url: "http://localhost:2000/productapi"
+  url: "http://localhost:2030/productapi"
 };
 
 export default config;
